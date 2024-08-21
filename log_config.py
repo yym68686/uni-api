bcf943401d9af015d964f8b631d2076aeb520b64
yym68686 <yym68686@outlook.com>
2024-08-22 02:38:56 +0800

    Simplify code

--- log_config.py
+++ log_config.py
@@ -2,5 +2,4 @@ import logging
 logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 logger = logging.getLogger("uni-api")
 
-httpx_logger = logging.getLogger("httpx")
-httpx_logger.setLevel(logging.CRITICAL)
+logging.getLogger("httpx").setLevel(logging.CRITICAL)
