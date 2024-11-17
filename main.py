c23690bb756ad1abd58a164ed257d61f3defa2f0
yym68686 <yym68686@outlook.com>
2024-11-18 06:31:46 +0800

    🐛 Bug: Fix the bug where tokens cannot be calculated.

--- main.py
+++ main.py
@@ -369,7 +369,7 @@ class LoggingStreamingResponse(Response):
             async for chunk in self.body_iterator:
                 if isinstance(chunk, str):
                     chunk = chunk.encode('utf-8')
-                if isinstance(chunk, bytes):
+                if self.current_info.get("endpoint") == "/v1/audio/speech":
                     yield chunk
                     continue
                 line = chunk.decode('utf-8')
