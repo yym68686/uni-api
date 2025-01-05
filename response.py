d39a1fdb880dde5531e50cd4c3fc904886c0645f
yym68686 <yym68686@outlook.com>
2025-01-05 13:31:52 +0800

    🐛 Bug: Fix the bug in the Gemini API where the response_format field is placed in the request body.
    ✨ Feature: Add feature: support displaying timeout duration in logs

--- response.py
+++ response.py
@@ -47,6 +47,7 @@ async def fetch_gemini_response_stream(client, url, headers, payload, model):
                         json_data = json.loads( "{" + line + "}")
                         content = json_data.get('text', '')
                         content = "\n".join(content.split("\\n"))
+                        # content = content.replace("\n", "\n\n")
                         if last_text_line == 0 and is_thinking:
                             content = "> " + content.lstrip()
                         if is_thinking:
