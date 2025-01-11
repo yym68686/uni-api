e0d6200447d7b21639fcc154678043f259db8d67
yym68686 <yym68686@outlook.com>
2025-01-12 01:47:55 +0800

    ✨ Feature: Completely shut down Gemini content review.

--- request.py
+++ request.py
@@ -217,19 +217,19 @@ async def get_gemini_payload(request, engine, provider):
         "safetySettings": [
             {
                 "category": "HARM_CATEGORY_HARASSMENT",
-                "threshold": "BLOCK_NONE"
+                "threshold": "OFF"
             },
             {
                 "category": "HARM_CATEGORY_HATE_SPEECH",
-                "threshold": "BLOCK_NONE"
+                "threshold": "OFF"
             },
             {
                 "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
-                "threshold": "BLOCK_NONE"
+                "threshold": "OFF"
             },
             {
                 "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
-                "threshold": "BLOCK_NONE"
+                "threshold": "OFF"
             }
         ]
     }
